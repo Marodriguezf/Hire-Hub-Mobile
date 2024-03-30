1ad2650f49a958ec0781667b43d18a373118a45b
Alejandra <alejandra.franco1290@gmail.com>
2024-03-29 22:37:56 -0500

    Upload UI mockups

---
+++ app/src/main/java/com/example/hirehub/registrar_candidatos.kt
@@ -0,0 +1,11 @@
+package com.example.hirehub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class registrar_candidatos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registrar_candidatos)
+    }
+}
